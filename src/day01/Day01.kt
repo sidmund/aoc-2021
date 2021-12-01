d4cfcdc070c52d3f82660cf0898bed949d54e75d
sidmund <sidrosterij@gmail.com>
2021-12-01 06:56:43 +0100

    Solve Day01

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -2,20 +2,40 @@ package day01
 
 import readInput
 
+fun part1(input: List<String>): Int {
+    /*var count = 0
+    for (index in input.indices.filter { it > 0 }) {
+        if (input[index].toInt() > input[index - 1].toInt()) {
+            count++
+        }
+    }
+    return count
+    */
+    return input.indices.filter { it > 0 && input[it].toInt() > input[it - 1].toInt() }.size
+}
+
+fun part2(input: List<String>): Int {
+    var count = 0
+    var prevSum = 0
+    for (index in input.indices) {
+        val a = input[index].toInt()
+        val b = if (index + 1 >= input.size) 0 else input[index + 1].toInt()
+        val c = if (index + 2 >= input.size) 0 else input[index + 2].toInt()
+        val sum = a + b + c
+        if (prevSum != 0 && sum > prevSum) {
+            count++
+        }
+        prevSum = sum
+    }
+    return count
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    //val testInput = readInput("day01/Day01_test")
+    //check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val input = readInput("day01/input")
     println(part1(input))
     println(part2(input))
 }
