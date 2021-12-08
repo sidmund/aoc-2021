868aa1b2944e582cae4c1162874f4e8da4afd817
sidmund <sidrosterij@gmail.com>
2021-12-08 13:53:14 +0100

    Implement general time measuring function

--- src/Utils.kt
+++ src/Utils.kt
@@ -16,3 +16,16 @@ fun readInputAsInt(filename: String) = File("src", "$filename.txt").readLines().
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+/**
+ * Measure execution time in ms of supplied function.
+ * @param logger a lambda expression to further process the elapsed time
+ * @param function the function to measure the time of
+ * @return the result of function
+ */
+inline fun <T> measure(logger: (Long) -> Unit, function: () -> T): T {
+    val start = System.currentTimeMillis()
+    val result: T = function.invoke()
+    logger.invoke(System.currentTimeMillis() - start)
+    return result
+}
